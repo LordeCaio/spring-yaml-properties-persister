a8ae1a80c613d7cd18eb67b838e27a32a5248400
Caio Macedo <caiomacedo0104@gmail.com>
2025-09-25 16:54:05 -0300

    initial commit

---
+++ src/main/java/br/com/lordecaio/springyamlpropertiespersister/SpringYamlPropertiesPersisterApplication.java
@@ -0,0 +1,12 @@
+package br.com.lordecaio.springyamlpropertiespersister;
+
+class SpringYamlPropertiesPersisterApplication {
+
+	public static void main(String[] args) {
+		System.out.println("Spring YAML Properties Persister Application Started");
+	}
+
+	private SpringYamlPropertiesPersisterApplication() {
+		throw new UnsupportedOperationException("This class cannot be instantiated");
+	}
+}
