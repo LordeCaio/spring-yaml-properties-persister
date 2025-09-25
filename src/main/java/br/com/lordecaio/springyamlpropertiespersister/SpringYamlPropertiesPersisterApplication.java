acb268c291d7c39813bb18dc8e416eecd465f906
Caio Macedo <caiomacedo0104@gmail.com>
2025-09-25 20:27:06 -0300

    fix: project build

--- src/main/java/br/com/lordecaio/springyamlpropertiespersister/SpringYamlPropertiesPersisterApplication.java
+++ src/main/java/br/com/lordecaio/springyamlpropertiespersister/SpringYamlPropertiesPersisterApplication.java
@@ -1,6 +1,6 @@
 package br.com.lordecaio.springyamlpropertiespersister;
 
-class SpringYamlPropertiesPersisterApplication {
+public class SpringYamlPropertiesPersisterApplication {
 
 	public static void main(String[] args) {
 		System.out.println("Spring YAML Properties Persister Application Started");
